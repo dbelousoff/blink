9ca93cdcb8b44d53e41579ee1ec6129e0b0a09c2
Dmitry Belousov <dmitry.belousoff@gmail.com>
2021-02-20 01:00:30 +0700

    Create Tender.js

---
+++ Tender.js
@@ -0,0 +1,26 @@
+black       = '#666666';
+red         = '#F43753'; // red
+green       = '#C9D05C'; // green
+yellow      = '#FFC24B'; // yellow
+blue        = '#B3DEEF'; // blue
+magenta     = '#D3B987'; // pink
+cyan        = '#73CEF4'; // cyan
+white       = '#EEEEEE'; // light gray
+lightBlack  = '#666666'; // medium gray
+lightRed    = '#F43753'; // red
+lightGreen  = '#C9D05C'; // green
+lightYellow = '#FFC24B'; // yellow
+lightBlue   = '#B3DEEF'; // blue
+lightMagenta= '#D3B987'; // pink
+lightCyan   = '#73CEF4'; // cyan
+lightWhite  = '#FEFFFF'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', '#EEEEEE');
+t.prefs_.set('foreground-color', '#FFC24B');
+t.prefs_.set('background-color', '#2662626');
